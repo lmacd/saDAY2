d153683d3bafb5695eb896aedf46be9bea9f96c8
lmacd <langstonmacdiarmid@gmail.com>
2013-10-29 00:05:17 -0400

    Added Timeline

--- js/timeline_final.js
+++ js/timeline_final.js
@@ -7,18 +7,10 @@ $(document).ready(function() {
     hi = $(window).height();
     wi = $(window).width();
 
-
-
-    /*   $('.left, .leftStandard').css({
-     'margin-left': hi * .05
-     });
-     
-     $('.right, .rightStandard').css({
-     'margin-left': (wi / 2)
-     }); */
-
     $(' .wrapper, #intro').css({'height': hi + 'px'});
-    $('#end').css({'height': hi / 4 + 'px'});
+    $('#end,#endM').css({'height': hi / 4 + 'px'});
+    var scale= (hi/680);
+    $('#skrollr-body').css({'font-size':16*scale+'px'});
 
     var year1 = $("#wrapper1").attr("title");
     var year2 = $("#wrapper2").attr("title");
@@ -29,30 +21,15 @@ $(document).ready(function() {
     var year7 = $("#wrapper7").attr("title");
     var year8 = $("#wrapper8").attr("title");
     var year9 = $("#wrapper9").attr("title");
+    var year10=$("#end").attr("title");
 
 
-    $("#top").click(function() {
+    $("#top,#topM").click(function() {
         $(window).scrollTo('0%', 1000);
         $('#year').css({'top': 0 + '%'});
 
     });
-    /*
-     $("#yearWrapper").click(function() {
-     var yPos = event.clientY;
-     alert('yPos');
-     var yW=$('#yearWrapper').innerHeight();
-     
-     $(document).scrollTo();
-     });
-     
-     
-     
-     
-     $(window).mouseup(function(){
-     $('#year').css({'background-color':'white'});
-     
-     });*/
-
+ 
     $(window).scroll(function() {
         var st = $(this).scrollTop();
         if ((st / hi) < 1)
@@ -62,31 +39,28 @@ $(document).ready(function() {
             $('#year').css({'margin-top': 0 + '%'});
 
         }
+        
         if ((st / hi) >= 1)
         {
             $('#year').show();
             $('.date').show();
         }
+        
         if ((st / hi) === 1)
         {
-
             var p = (year1 - 1970) / 45;
-            //alert(year1)
             $('#year').animate({'top': (p * 100) + 0 + '%'}, 700);
         }
 
         else if ((st / hi) === 2)
         {
-
             var p = (year2 - 1970) / 45;
-            //alert(p*100);
             $('#year').animate({'top': (p * 100) - 2 + '%'}, 700);
         }
 
         else if ((st / hi) === 3)
         {
             var p = (year3 - 1970) / 45;
-            //alert(p*100);
             $('#year').animate({'top': (p * 100) - 3 + '%'}, 700);
         }
 
@@ -94,49 +68,43 @@ $(document).ready(function() {
         {
             var p = (year4 - 1970) / 45;
             $('#year').animate({'top': (p * 100) - 4 + '%'}, 700);
-            //                      alert($('#yearWrapper').css('height'));
-
-            //alert($('#year').css('top'));
-
         }
 
         else if ((st / hi) === 5)
         {
             var p = (year5 - 1970) / 45;
             $('#year').animate({'top': (p * 100) - 3 + '%'}, 700);
-            //     alert($('#year').css('top'));
-
         }
 
         else if ((st / hi) === 6)
         {
             var p = (year6 - 1970) / 45;
             $('#year').animate({'top': (p * 100) - 3.5 + '%'}, 700);
-            //     alert($('#year').css('top'));
-
         }
 
         else if ((st / hi) === 7)
         {
             var p = (year7 - 1970) / 45;
             $('#year').animate({'top': (p * 100) - 4 + '%'}, 700);
-            // alert($('#year').css('top'));
         }
 
         else if ((st / hi) === 8)
         {
             var p = (year8 - 1970) / 45;
             $('#year').animate({'top': (p * 101) - 7 + '%'}, 700);
-            //      alert($('#year').css('top'));
-
         }
 
         else if ((st / hi) === 9)
         {
             var p = (year9 - 1970) / 45;
-            //alert(p*100);
             $('#year').animate({'top': (p * 101) - 3 + '%'}, 700);
         }
+        
+     /*  else if((st / hi)==9.3)
+             {
+            var p = (year10 - 1970) / 45;
+            $('#year').animate({'top': (p * 101) - 3 + '%'}, 700);
+        }*/
 
     });
 
